dcf3dba1ba82435cca5af454be88b31911e1b776
jmgallen <35116410+jmgallen@users.noreply.github.com>
2022-09-24 22:12:57 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// Jeremy Gallen's sample program to demonstrate event handlers
+
+// Overall namespace
+namespace EventHandlers
+{
+    // Public delegate
+    public delegate string MyDelegate(string str);
+    // Main class
+    class EventProgramExample
+    {
+        // Event declaration
+        event MyDelegate MyEvent;
+
+        // Event program sample
+        public EventProgramExample()
+        {
+            // Add to event
+            this.MyEvent += new MyDelegate(this.WelcomeUser);
+        }
+
+        // Method to welcome user
+        public string WelcomeUser(string username)
+        {
+            return "Welcome, " + username + ".";
+        }
+
+        // Main method
+        static void Main(string[] args)
+        {
+            // Create event sample and output
+            EventProgramExample object1 = new EventProgramExample();
+            string outcome = object1.MyEvent("Jeremy");
+            Console.WriteLine(outcome);
+        }
+    }
+}
